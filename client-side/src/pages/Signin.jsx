b22e138a2cc8c1b301fec26747732800af2f2dfd
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-03-05 20:50:15 +0530

    added Oauth functionality Google SIgnin and Signup

--- client-side/src/pages/Signin.jsx
+++ client-side/src/pages/Signin.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice';
 import {useDispatch,useSelector } from "react-redux";
+import Oauth from "../components/Oauth";
 
 
 
@@ -95,6 +96,7 @@ export default function Signin() {
                 'Sign In'
               )}
             </Button>
+            <Oauth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account ?</span>
